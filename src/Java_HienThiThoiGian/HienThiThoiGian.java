a64a745b81bd1f86f0e47013d2c9ff88e0902e76
Son <trcongson.tcs@gmail.com>
2018-10-04 09:54:09 +0700

    first cm

---
+++ src/Java_HienThiThoiGian/HienThiThoiGian.java
@@ -0,0 +1,11 @@
+package Java_HienThiThoiGian;
+
+import java.util.Date;
+
+public class HienThiThoiGian {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println("Now is " + now);
+    }
+}
